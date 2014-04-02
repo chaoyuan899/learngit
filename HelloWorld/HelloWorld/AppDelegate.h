62ece70f644425e095394a479e8fa0837665090a
chaoyuan899 <782428485@qq.com>
2014-04-02 13:42:05 +0800

    add HelloWorld Project

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Eastern Phoenix on 2/4/14.
+//  Copyright (c) 2014 The Technology Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Eastern Phoenix on 2/4/14.
+//  Copyright (c) 2014 The Technology Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
